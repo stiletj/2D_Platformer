ceba1803e2217b287dbcdb58087e70690903e407
stiletj <tjstiles440@gmail.com>
2024-06-23 22:16:18 +0800

    tested new scene manager

--- Platformer/Assets/Scripts/Scene Managers/SM8to9.cs
+++ Platformer/Assets/Scripts/Scene Managers/SM8to9.cs
@@ -1,18 +1,45 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class SM8to9 : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    //public string scene;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    //private Vector2 momentum;
+    //public static Vector2 momentumEightEnd;
+    //public GameObject player;
+    //public Transform spawnPoint;
+
+    //PlayerMove playerMove;
+
+    //void Start()
+    //{
+    //    //sm2to1 = twotoone.GetComponent<SM2to1>();
+
+    //    if (SM9to8.momentumNine != new Vector2(0f, 0f))
+    //    {
+    //        player.transform.position = spawnPoint.position;
+    //    }
+
+    //    playerMove = player.GetComponent<PlayerMove>();
+
+    //    playerMove.controller.velocity = SM9to8.momentumNine;
+
+    //    SM9to8.momentumNine = new Vector2(0f, 0f);
+    //}
+
+    //void Update()
+    //{
+    //    momentum = playerMove.controller.velocity;
+    //}
+
+    //void OnTriggerEnter2D(Collider2D collider)
+    //{
+    //    momentumEightEnd = momentum;
+    //    //Debug.Log(momentum);
+
+    //    SceneManager.LoadScene(scene);
+    //}
 }
